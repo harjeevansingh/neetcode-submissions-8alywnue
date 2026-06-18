c6b63465a7b021a4658625315f8acedbedbccfc3
Harjeevan Singh <harjeevansingh01@gmail.com>
2026-06-19 00:00:43 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int length = s.length();
+
+        int left = 0;
+        int right = 0;
+
+        int result = 0;
+
+        Map<Character, Integer> counter = new HashMap<>();
+
+        while(left<=right && right<length){
+            char currentRightChar = s.charAt(right);
+            counter.put(currentRightChar, counter.computeIfAbsent(currentRightChar, j -> 0) + 1);
+
+            while((right - left + 1) - Collections.max(counter.values()) > k){
+                char currentLeftChar = s.charAt(left);
+                counter.put(currentLeftChar, counter.get(currentLeftChar) - 1);
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+
+            right++;
+
+        }
+
+        return result;
+    }
+}

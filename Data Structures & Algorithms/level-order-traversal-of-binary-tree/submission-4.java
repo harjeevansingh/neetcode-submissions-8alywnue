b1e42281ab8b23936309c9932b9b72669dde0469
Harjeevan Singh <harjeevansingh01@gmail.com>
2026-06-29 21:38:32 +0530

    Add: level-order-traversal-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-4.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            List<List<Integer>> leveledNodes = new ArrayList<>();
+
+            while(!queue.isEmpty()){
+            
+                int levelSize = queue.size();
+                List<Integer> currentLevelNodes = new ArrayList<>();
+
+                for(int i=0;i<levelSize;i++){
+                    TreeNode currentNode = queue.poll();
+                    if(currentNode != null){
+                        currentLevelNodes.add(currentNode.val);
+                        queue.add(currentNode.left);
+                        queue.add(currentNode.right);
+                    }
+                }
+
+                if(!currentLevelNodes.isEmpty())
+                    leveledNodes.add(currentLevelNodes);
+            }
+
+            return leveledNodes;
+
+        //  List<List<Integer>> res = new ArrayList<>();
+
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.add(root);
+
+        // while (!q.isEmpty()) {
+        //     List<Integer> level = new ArrayList<>();
+
+        //     for (int i = q.size(); i > 0; i--) {
+        //         TreeNode node = q.poll();
+        //         if (node != null) {
+        //             level.add(node.val);
+        //             q.add(node.left);
+        //             q.add(node.right);
+        //         }
+        //     }
+        //     if (level.size() > 0) {
+        //         res.add(level);
+        //     }
+        // }
+        // return res;
+    }
+
+}

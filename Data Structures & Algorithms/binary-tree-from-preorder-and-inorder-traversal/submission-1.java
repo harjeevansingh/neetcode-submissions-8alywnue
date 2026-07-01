1e6e5249d7c5acc370b31308c7d961649dc053f4
Harjeevan Singh <harjeevansingh01@gmail.com>
2026-07-02 01:02:29 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder == null || preorder.length == 0){
+            return null;
+        }
+
+        TreeNode currentNode = new TreeNode(preorder[0]);
+
+        if(preorder.length == 1){
+            return currentNode;
+        }
+
+        int length = preorder.length;
+        int root = preorder[0];
+        int lengthOrLeftSuBtree = 0;
+        for(int i=0;i<length;i++){
+            if(inorder[i] == root){
+                lengthOrLeftSuBtree = i;
+            }
+        }
+
+        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, lengthOrLeftSuBtree+1);
+        int[] leftInorder = Arrays.copyOfRange(inorder, 0, lengthOrLeftSuBtree);
+
+        int[] rightPreorder = Arrays.copyOfRange(preorder, lengthOrLeftSuBtree+1, length);
+        int[] rightInorder = Arrays.copyOfRange(inorder, lengthOrLeftSuBtree+1, length);
+
+        TreeNode leftSubTree = buildTree(leftPreorder, leftInorder);
+        TreeNode rightSubTree = buildTree(rightPreorder, rightInorder);
+
+        currentNode.left = leftSubTree;
+        currentNode.right = rightSubTree;
+
+        return currentNode;
+        
+    }
+}
